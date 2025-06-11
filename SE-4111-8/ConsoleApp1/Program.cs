989a573cbf2a4570c7d315ba255a25b1a99a67a1
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2025-06-11 22:00:05 +0400

    Data types

---
+++ SE-4111-8/ConsoleApp1/Program.cs
@@ -0,0 +1,88 @@
+﻿using System.Xml.Linq;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //ტექსტური "Nika"
+            // "asdasd"  'A' ???
+
+
+            //რიცხვითი
+            // მთელი , ათწილადი
+
+
+            //ლოგიკური
+            //true || false
+
+
+            //Console.WriteLine("Nika");
+            //Console.WriteLine('A');
+            //Console.WriteLine(false);
+            //Console.WriteLine(100);
+            //Console.WriteLine(100.1);
+
+
+            //string name = "Nika";
+            //string name = Console.ReadLine();
+            //string lastname = Console.ReadLine();
+
+
+            //Console.WriteLine($"My name is: {name}");
+            //Console.WriteLine("My name is: {0} {1}", name, lastname);
+            //Console.WriteLine("My name is:" + name);
+
+            //char firstSymbol = 'N';
+
+            //byte age = 255;
+            //sbyte age2 = -128;
+
+            //short x = -1;
+            //ushort x2 = 1;
+
+            //int x = 12;
+            //uint x2 = 12;
+
+            //long x = 12;
+            //long x2 = 12;
+
+            //float x = 1.2f;
+            //double y = 1.0;
+            //decimal z = 2.2m;
+
+            //bool x = true;
+
+
+
+
+
+            //1.შექმენით კონსოლური აპლიკაცია
+
+            //2.შექმენით სარეგისტრაციო ფორმა სადაც მომხმარებელს სთხოვთ მისი ინფორმაციის შემოყვანას კონსოლიდან შემდეგი ფორმატით:
+            // FirstName: <-------- ინფორმაცია შემოდის კონსოლიდან
+            // LastName: <-------- ინფორმაცია შემოდის კონსოლიდან
+            // Age: <-------- ინფორმაცია შემოდის კონსოლიდან
+
+
+            //3. ინფორმაცია დაბეჭდეთ კონსოლში შემდეგი ფორმატით:
+            //Hello my name is {FirstName} {LastName} I am {Age} years old
+
+
+            //Console.Write("FirstName: ");
+            //string firstName = Console.ReadLine();
+
+            //Console.Write("LastName: ");
+            //string lastName = Console.ReadLine();
+
+            //Console.Write("Age: ");
+            //byte age = Convert.ToByte(Console.ReadLine());
+            ////byte age = byte.Parse(Console.ReadLine());
+
+            //Console.WriteLine($"Hello my name is {firstName} {lastName} I am {age} years old");
+        }
+
+    }
+}
