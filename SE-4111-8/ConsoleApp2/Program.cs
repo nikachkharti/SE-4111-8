1780e241c3a79990e532d2af09b5b363f898b84c
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2025-06-30 21:59:45 +0400

    Add new methods to Program class for array operations
    - Updated Main method to initialize an array and call FirstOrDefault.
    - Added FirstOrDefault method to search for an element in an array.
    - Introduced Min method to find the minimum value in an array.
    - Implemented ToFarenheit method for Celsius to Fahrenheit conversion.
    - Created PrintHelloWorld method to print greetings multiple times.
    - Added PrintHi method for personalized greetings.
    - Implemented IsFunctionPalindrome method to check for palindromes in an array.

--- SE-4111-8/ConsoleApp2/Program.cs
+++ SE-4111-8/ConsoleApp2/Program.cs
@@ -1,7 +1,73 @@
-﻿internal class Program
+﻿
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int[] array = [10, 1, 2, 13, 44, 13];
+
+
+        var result = FirstOrDefault(array, 132);
+
     }
+
+
+    private static int FirstOrDefault(int[] collection, int element)
+    {
+        for (int i = 0; i < collection.Length; i++)
+        {
+            if (element == collection[i])
+            {
+                return collection[i];
+            }
+        }
+
+        return default;
+    }
+    private static int Min(int[] array)
+    {
+        int min = array[0];
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] < min)
+            {
+                min = array[i];
+            }
+        }
+
+        return min;
+    }
+    static double ToFarenheit(double celsius)
+    {
+        return (celsius * 9 / 5) + 32;
+    }
+    static void PrintHelloWorld(int count)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    static void PrintHi(string name)
+    {
+        Console.WriteLine($"Hi {name}");
+    }
+    static bool IsFunctionPalindrome(int[] collection)
+    {
+        int start = 0;
+        int end = collection.Length - 1;
+
+        while (start < end)
+        {
+            if (collection[start] != collection[end])
+            {
+                return false;
+            }
+
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+
 }
