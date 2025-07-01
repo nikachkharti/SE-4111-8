5320316bf32676e6d00f51c74a9c51b73fdb8fe0
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2025-07-01 21:43:23 +0400

    Add LectureStrings project and update solution
    - Included new project `LectureStrings` in `SE-4111-8.sln`.
    - Updated Visual Studio version to 17.14.36202.13.
    - Created `LectureStrings.csproj` with SDK-style format targeting .NET 9.0.
    - Added `Program.cs` with examples of string and character manipulations.

---
+++ SE-4111-8/LectureStrings/Program.cs
@@ -0,0 +1,93 @@
+﻿using System.Globalization;
+using System.Text;
+
+namespace LectureStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region STRING
+
+            //string name = "Nikoloz Chkhartishvili";
+            //string lastName = "CHKHARTISHVILI";
+            //double x = 12.2;
+
+            //string nameToUpper = name.ToUpper(); // ტექსტის მაღალ რეგისტრში გადააყვანა
+            //string nameToLower = name.ToLower(); // ტექსტის დაბალ რეგისტრში გადააყვანა
+            //int nameLength = name.Length; // ტექსტის ზომის გაგება
+
+            //string fullName = name + " " + lastName; // ტექსტის გაერთიანება
+            //string fullName = $"{name} {lastName}"; // ტექსტის გაერთიანება
+
+
+            //ტექსტის გაერთიანება მოსახერხებელია მხოლოდ დიდი მოცულობის ტექსტებთან სამუშაოდ !!!
+            //StringBuilder sb = new StringBuilder();
+            //sb.Append(name);
+            //sb.Append(" ");
+            //sb.Append(lastName);
+
+            //Console.WriteLine(sb);
+
+
+            //Console.WriteLine(name == lastName); // ტექსტის შედარება
+            //Console.WriteLine(name.Equals(lastName, StringComparison.OrdinalIgnoreCase)); // ტექსტის შედარება + case დაიგნორება
+            //Console.WriteLine(name.Equals(lastName, StringComparison.InvariantCultureIgnoreCase)); // ტექსტის შედარება + კულტურის დაიგნორება
+
+            //string subName = name.Substring(0, 4); // ქვეტექსტის ამოღება
+
+
+            //string convertedX = x.ToString(); // ნებისმიერი მნიშვნელობის ტექსტად გარდაქმნა
+
+
+            //string trimmedName = name.Trim(); // თავსა და ბოლოში დაწერილ სფეისებს დააიგნორებს
+            //string trimmedName = name.TrimStart(); // თავში დაწერილ სფეისებს დააიგნორებს
+            //string trimmedName = name.TrimEnd(); // ბოლოშ დაწერილ სფეისებს დააიგნორებს
+
+            //string[] separatedInfo = name.Split(' '); // რაიმე პატერნით ტექსტის დახლეჩა
+
+
+            //bool chkhExists = name.Contains("Chkh"); // ქვეტექსტის ძებნა ტექსტში
+
+
+            //string replacedName = name.Replace('N', 'z'); // ტექსტის ჩანაცვლება
+            //string replacedName = name.Replace("n", "z", StringComparison.OrdinalIgnoreCase); // ტექსტის ჩანაცვლება
+
+            //var a = name.IndexOfAny(new char[] { 'N', 'o' }, 0); // ინდექსის ძებნა სტრინგში
+            //var a = name.LastIndexOfAny(new char[] { 'N', 'o' }, 0); // ინდექსის ძებნა სტრინგში 
+            #endregion
+
+
+            #region CHAR
+            //string name = "Nika";
+
+            //for (int i = 0; i < name.Length; i++)
+            //{
+            //    char symbolOfText = name[i];
+            //}
+
+            //char symbol = 'Z'; // ---> Z 
+            //int symbolIntValue = 'Z'; // ---> 90
+            //var sum = '1' + '1'; // --> 98
+
+
+            //Console.WriteLine(Char.IsDigit(symbol)); // ამოწმებს სიმბოლო არის თუ არა რიცხვითი
+            //Console.WriteLine(Char.IsLetter(symbol)); // ამოწმებს სიმბოლო არის თუ არა ასო
+            //Console.WriteLine(Char.IsUpper(symbol)); // ამოწმებს სიმბოლო არის თუ არა მაღალ რეგისტრში
+            //Console.WriteLine(Char.IsLower(symbol)); // ამოწმებს სიმბოლო არის თუ არა დაბალ რეგისტრში
+
+            //Console.WriteLine(Char.ToLower(symbol)); // z
+            //Console.WriteLine(Char.ToUpper(symbol)); // z
+
+
+            //for (char c = 'A'; c <= 'Z'; c++)
+            //{
+            //    Console.WriteLine($"Character: {c}, Unicode: {Convert.ToInt32(c)}");
+            //}
+
+
+            #endregion
+
+        }
+    }
+}
