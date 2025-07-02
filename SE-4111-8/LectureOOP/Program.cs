9b3417756572a1d03459b07a607ffd60df662f25
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2025-07-02 21:37:55 +0400

    Add LectureOOP project with Person class implementation
    - Introduced a new project "LectureOOP" in the solution.
    - Created `LectureOOP.csproj` with .NET SDK and necessary properties.
    - Added `Person` class in `Program.cs` with fields, constructors, and methods.
    - Demonstrated class usage in `Main` method with multiple instances.
    - Included comments in Georgian for clarity and context.
    - Added commented-out code and a block comment for future reference.

---
+++ SE-4111-8/LectureOOP/Program.cs
@@ -0,0 +1,96 @@
+﻿namespace LectureOOP
+{
+    public class Person
+    {
+        public string name;
+        public int age;
+
+        //კონსტრუქტორი
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        //კონსტრუქტორი
+        public Person()
+        {
+        }
+
+        //კონსტრუქტორი
+        public Person(int age)
+        {
+            this.age = age;
+        }
+
+        public void Walk()
+        {
+            Console.WriteLine($"{name} WALKING...");
+        }
+
+        public void Talk()
+        {
+            Console.WriteLine($"My Name is {name} I am {age} years old");
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person nikasObj = new Person() /*<--კონსტრუქტორი*/;
+            nikasObj.name = "Nika";
+            nikasObj.age = 16;
+
+            nikasObj.Walk();
+            nikasObj.Talk();
+
+
+            Person giorgisObj = new Person() /*<--კონსტრუქტორი*/
+            {
+                name = "Giorgi",
+                age = 20
+            };
+            giorgisObj.Walk();
+            giorgisObj.Talk();
+
+
+
+            Person elenesObj = new Person("Elene", 25); /*<--კონსტრუქტორი*/
+            elenesObj.Walk();
+            elenesObj.Talk();
+
+
+            Person davitisObj = new Person(33); /*<--კონსტრუქტორი*/
+            davitisObj.name = "Daviti";
+
+            davitisObj.Walk();
+            davitisObj.Talk();
+
+
+
+
+            //Console.WriteLine($"{nikasObj.name} {nikasObj.age}");
+            //Console.WriteLine($"{giorgisObj.name} {giorgisObj.age}");
+
+
+
+
+
+            /*
+             
+            let obj1 = {name:"Nika",age:12},
+
+            [
+                {name:"Nika",age:12},
+                {name:"Giorgi",age:13},
+                {name:"Elene",age:45}
+            ] 
+
+             */
+
+        }
+    }
+}
