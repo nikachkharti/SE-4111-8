195c884684f4281c39ec230dc383b229202d73ab
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2025-07-05 11:05:14 +0400

    Add TinyBank projects and customer repository implementation
    - Introduced "Concurrency", "TinyBank.Models", "TinyBank.Data", "TinyBank.Repository", "TinyBank.Service", and "TinyBank.Tests" projects to the solution.
    - Updated solution configuration for new projects.
    - Created `ICustomerRepository` interface for customer management.
    - Implemented `CustomerRepository` class for data handling, including file operations.
    - Updated `TinyBank.Repository.csproj` to target .NET 9.0 and added necessary project references.

---
+++ SE-4111-8/TinyBank.Repository/Contracts/ICustomerRepository.cs
@@ -0,0 +1,13 @@
+﻿using TinyBank.Models;
+
+namespace TinyBank.Repository.Contracts
+{
+    public interface ICustomerRepository
+    {
+        List<Customer> GetCustomers();
+        Customer GetSingleCustomer(int id);
+        int AddCustomer(Customer newCustomer);
+        int UpdateCustomer(Customer newCustomer);
+        int DeleteCustomer(int id);
+    }
+}
---
+++ SE-4111-8/TinyBank.Repository/Implementations/CustomerRepository.cs
@@ -0,0 +1,108 @@
+﻿using TinyBank.Models;
+using TinyBank.Repository.Contracts;
+
+namespace TinyBank.Repository.Implementations
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        private readonly string _filePath;
+        private List<Customer> _customers;
+
+        public CustomerRepository(string filePath)
+        {
+            _filePath = filePath;
+            _customers = LoadData();
+
+        }
+
+        public List<Customer> GetCustomers() => _customers;
+        public Customer GetSingleCustomer(int id) => _customers.FirstOrDefault(c => c.Id == id);
+        public int AddCustomer(Customer newCustomer)
+        {
+            newCustomer.Id = _customers.Any() ? _customers.Max(c => c.Id) + 1 : 1;
+            _customers.Add(newCustomer);
+            SaveData();
+
+            return newCustomer.Id;
+        }
+        public int DeleteCustomer(int id)
+        {
+            var customer = _customers.FirstOrDefault(person => person.Id == id);
+
+            if (customer != null)
+            {
+                _customers.Remove(customer);
+                SaveData();
+            }
+
+            return customer.Id;
+        }
+        public int UpdateCustomer(Customer customer)
+        {
+            var index = _customers.FindIndex(c => c.Id == customer.Id);
+
+            if (index >= 0)
+            {
+                _customers[index] = customer;
+                SaveData();
+            }
+
+            return customer.Id;
+        }
+
+
+
+        #region HELPERS
+        private List<Customer> LoadData()
+        {
+            if (!File.Exists(_filePath))
+                return new List<Customer>();
+
+            var customers = new List<Customer>();
+
+            using (var reader = new StreamReader(_filePath))
+            {
+                string line = string.Empty;
+
+                while ((line = reader.ReadLine()) != null)
+                {
+                    var customer = FromCsv(line);
+                    customers.Add(customer);
+                }
+            }
+
+            return customers;
+        }
+        private void SaveData()
+        {
+            using (var writer = new StreamWriter(_filePath, append: false)) // 'false' to overwrite the file
+            {
+                writer.WriteLine("Id,Name,IdentityNumber,PhoneNumber,Email,CustomerType");
+
+                foreach (var customer in _customers)
+                {
+                    writer.WriteLine(ToCsv(customer));
+                }
+            }
+        }
+        private Customer FromCsv(string customer)
+        {
+            var separatedCustomer = customer.Split(',', StringSplitOptions.RemoveEmptyEntries);
+            Customer result = new();
+
+            if (separatedCustomer.Length != 6)
+                throw new FormatException("Customer format is invalid");
+
+            result.Id = int.Parse(separatedCustomer[0]);
+            result.Name = separatedCustomer[1];
+            result.IdentityNumber = separatedCustomer[2];
+            result.PhoneNumber = separatedCustomer[3];
+            result.Email = separatedCustomer[4];
+            result.CustomerType = Enum.Parse<CustomerType>(separatedCustomer[5]);
+
+            return result;
+        }
+        private string ToCsv(Customer customer) => $"{customer.Id},{customer.Name},{customer.IdentityNumber},{customer.PhoneNumber},{customer.Email},{Convert.ToInt32(customer.CustomerType)}".Trim();
+        #endregion
+    }
+}
