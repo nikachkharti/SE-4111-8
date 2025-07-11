a8422ae7c7ca3b9ce881d0e0f0e64a7c99fe40a6
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2025-07-11 21:30:46 +0400

    datetime

--- SE-4111-8/LectureInterfacesDateTimeEnum/Program.cs
+++ SE-4111-8/LectureInterfacesDateTimeEnum/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace LectureInterfacesDateTimeEnum
+﻿using System.Security;
+
+namespace LectureInterfacesDateTimeEnum
 {
     internal class Program
     {
@@ -82,9 +84,48 @@
             }
         }
 
-
         static void Main(string[] args)
         {
+            //var currentDateTime = DateTime.Now;
+            //var today = DateTime.Today;
+            //var currentDateTimeUtc = DateTime.UtcNow;
+
+            //Console.WriteLine(today);
+            //Console.WriteLine(currentDateTime);
+            //Console.WriteLine(currentDateTimeUtc);
+
+            //DateTime customDate = new DateTime(year: 2025, month: 7, day: 11);
+            //DateTime customDateWithTime = new DateTime(year: 2023, month: 12, day: 25, hour: 14, minute: 30, second: 0);
+
+            //Console.WriteLine(customDate);
+            //Console.WriteLine(customDateWithTime);
+
+            //Console.WriteLine(customDate.DayOfWeek);
+            //Console.WriteLine(customDate.DayOfYear);
+
+            //Console.WriteLine(customDate.AddDays(1)); // უმატებს ერთ დღეს
+            //Console.WriteLine(customDate.AddDays(-1)); // აკლებს ერთ დღეს
+
+
+            //DateTime start = new DateTime(2023, 7, 11);
+            //DateTime end = DateTime.Now;
+
+            //TimeSpan diff = end - start;
+            //Console.WriteLine(diff);
+
+            //string currentDate = "2025-07-11";
+            //DateTime x = DateTime.Parse(currentDate);
+            //DateTime.TryParse(currentDate, out var x);
+
+            //DateTime now = DateTime.Now;
+
+            //Console.WriteLine(now.ToString("yyyy-MM-dd HH:mm"));
+            //Console.WriteLine(now.ToString("dddd,MMMM,dddd"));
+
+
+            //დაწერეთ ფუნქცია რომელსაც გადავცემთ, დაბადების თარიღს და ფუნქცია დაგვიბრუნებს თუ რამდენი წლისაა ადამიანი
+
+
         }
     }
 }
