8c077b7b4a4c7a83733c18594e865cc17b4b9ebf
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2025-07-14 20:59:20 +0400

    operator overlaod, ICompaarble

---
+++ SE-4111-8/LectureOperatorOverload/Money.cs
@@ -0,0 +1,50 @@
+﻿namespace LectureOperatorOverload
+{
+    public class Money : IComparable<Money>
+    {
+        public decimal Amount { get; set; }
+        public string Currency { get; set; }
+
+        public Money(decimal amount, string currency)
+        {
+            Amount = amount;
+            Currency = currency;
+        }
+
+
+        public static Money operator +(Money first, Money second)
+        {
+            ValidateSameCurrency(first, second);
+
+            var amountSum = first.Amount + second.Amount;
+            return new Money(amountSum, first.Currency);
+        }
+
+
+        public override string ToString() => $"{Amount} {Currency}";
+
+        public int CompareTo(Money other)
+        {
+            ValidateSameCurrency(other);
+            return Amount.CompareTo(other.Amount);
+        }
+
+
+        private void ValidateSameCurrency(Money other)
+        {
+            if (Currency != other.Currency)
+            {
+                throw new InvalidOperationException("Can't work with different currencies");
+            }
+        }
+        private static void ValidateSameCurrency(Money money1, Money money2)
+        {
+            if (money1.Currency != money2.Currency)
+            {
+                throw new InvalidOperationException("Can't work with different currencies");
+            }
+        }
+
+
+    }
+}
---
+++ SE-4111-8/LectureOperatorOverload/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace LectureOperatorOverload
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Money firstMoney = new(101, "USD");
+            Money secondMoney = new(102, "EUR");
+
+            //var a = firstMoney.CompareTo(secondMoney); //0  1  -1
+
+
+            //+ - * /
+
+            Console.WriteLine(firstMoney + secondMoney);
+
+            //Console.WriteLine(1 + 1) => 2;
+            //Console.WriteLine("1" + "1") => "11";
+
+        }
+    }
+}
