47560c5cfb5ca2e6b0dce7976fd31f353734dd80
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2025-07-16 21:50:25 +0400

    Vehicles

--- SE-4111-8/LectureVehiclesPractice/Program.cs
+++ SE-4111-8/LectureVehiclesPractice/Program.cs
@@ -5,8 +5,23 @@
         static void Main(string[] args)
         {
             string[] vehiclesString = File.ReadAllLines(@"../../../vehicles.csv");
+            Vehicle[] cars = TransformToCars(vehiclesString);
 
-            //1. ამ ფაილში არსებული თითოეული მასივის ელემენტი, გარდაქმენით მანქანის ობიექტად და დაბეჭდეთ კონსოლში
+            //1. ამ ფაილში არსებული თითოეული მასივის ელემენტი, გარდაქმენით მანქანის ობიექტად და დაბეჭდეთ კონსოლში +
+            //2. მოძებნეთ ყველა მერსედეს ბენცი.
+            //3. მოძებნეთ 10 ყველაზე ეკონომიური მანქანა.
+        }
+
+        private static Vehicle[] TransformToCars(string[] vehiclesString)
+        {
+            Vehicle[] cars = new Vehicle[vehiclesString.Length];
+
+            for (int i = 1; i < vehiclesString.Length; i++)
+            {
+                cars[i] = Vehicle.Parse(vehiclesString[i]);
+            }
+
+            return cars;
         }
     }
 }
---
+++ SE-4111-8/LectureVehiclesPractice/Vehicle.cs
@@ -0,0 +1,41 @@
+﻿namespace LectureVehiclesPractice
+{
+    public class Vehicle
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public byte Cylinder { get; set; }
+        public float Engine { get; set; }
+        public string Drive { get; set; }
+        public string Transmission { get; set; }
+        public byte City { get; set; }
+        public byte Combined { get; set; }
+        public byte Highway { get; set; }
+
+        public override string ToString() => $"{Make} {Model} {Cylinder} {Engine} {Drive} {Transmission} {City} {Combined} {Highway}";
+
+        public static Vehicle Parse(string data)
+        {
+            string[] separatedData = data.Split(',');
+
+            if (separatedData.Length != 9)
+                throw new FormatException("Invalid argument passed");
+
+            Vehicle result = new()
+            {
+                Make = separatedData[0],
+                Model = separatedData[1],
+                Cylinder = byte.Parse(separatedData[2]),
+                Engine = float.Parse(separatedData[3]),
+                Drive = separatedData[4],
+                Transmission = separatedData[5],
+                City = byte.Parse(separatedData[6]),
+                Combined = byte.Parse(separatedData[7]),
+                Highway = byte.Parse(separatedData[8])
+            };
+
+            return result;
+        }
+
+    }
+}
