f1e4e2c61459fe94d0e99683154d5bec0f058a55
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2025-07-16 20:28:04 +0400

    Vehicles csv data

---
+++ SE-4111-8/LectureVehiclesPractice/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace LectureVehiclesPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vehiclesString = File.ReadAllLines(@"../../../vehicles.csv");
+
+            //1. ამ ფაილში არსებული თითოეული მასივის ელემენტი, გარდაქმენით მანქანის ობიექტად და დაბეჭდეთ კონსოლში
+        }
+    }
+}
