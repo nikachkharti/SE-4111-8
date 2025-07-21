1ebc3d969ea37ec15f3d9e6d36f2e32da545967b
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2025-07-21 21:14:38 +0400

    Delegates, anonymus functions

--- SE-4111-8/LectureDataStructures/Algorithms.cs
+++ SE-4111-8/LectureDataStructures/Algorithms.cs
@@ -1,6 +1,36 @@
 ﻿namespace LectureDataStructures
 {
+    public delegate bool Predicate(int value);
+
     public class Algorithms
     {
+        //მოვძებნოთ პირველივე ელემენტი
+        public static int CustomFirstOrDefault(List<int> intList, Predicate comparerDelegate) //<< piroba
+        {
+            for (int i = 0; i < intList.Count; i++)
+            {
+                if (comparerDelegate(intList[i]))
+                {
+                    return intList[i];
+                }
+            }
+
+            return default;
+        }
+
+        public static int CustomLastOrDefault(int[] numbersArray, Predicate predicate)
+        {
+            for (int i = numbersArray.Length - 1; i >= 0; i--)
+            {
+                if (predicate(numbersArray[i]))
+                {
+                    return numbersArray[i];
+                }
+            }
+
+            return default;
+        }
+
+
     }
 }
--- SE-4111-8/LectureDataStructures/Program.cs
+++ SE-4111-8/LectureDataStructures/Program.cs
@@ -1,27 +1,37 @@
-﻿namespace LectureDataStructures
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace LectureDataStructures
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            #region LIST
-            int[] ar = [98, 11, 44, -10];
+            int[] intAr = { 1, 11, -2, 21, 30, -7, 21, 30 };
+            List<int> intList = new() { 1, -11, 2, 21, 30, 7 };
 
-            List<int> intList = new List<int>();
-            intList.Add(10); // ამატებს ერთ ელემენტს
-            intList.Add(300); // ამატებს ერთ ელემენტს
-            intList.Add(788); // ამატებს ერთ ელემენტს
-            intList.Add(9); // ამატებს ერთ ელემენტს
-            intList.Add(19); // ამატებს ერთ ელემენტს
-                             //intList.AddRange(ar); // ამატებს რამდენიმე ელემენტს
-                             //intList.Remove(10);
-                             //intList.RemoveRange(0, 2); // წაშალე პირველი ორი ელემენტი ლისტში
-                             //intList.Clear(); // ასუფთავებს ლისტს
-                             //intList.Insert(0, 99); // ლისტის ნებისმიერ ინდექსზე ნებისმიერი მნიშვნელობის ჩამატება
-                             //intList.InsertRange(0, [7, 7, 7]);
-                             //bool contains = intList.Contains(7);
-                             //int idx = intList.IndexOf(-10);
-                             //intList.TrimExcess(); // გაამადგურებს ტყუილად გამოყოფილ ადგილებს
+            var result = Algorithms.CustomFirstOrDefault(intList, number => number % 2 == 0);
+
+
+
+
+            #region LIST
+            //int[] ar = [98, 11, 44, -10];
+
+            //List<int> intList = new List<int>();
+            //intList.Add(10); // ამატებს ერთ ელემენტს
+            //intList.Add(300); // ამატებს ერთ ელემენტს
+            //intList.Add(788); // ამატებს ერთ ელემენტს
+            //intList.Add(9); // ამატებს ერთ ელემენტს
+            //intList.Add(19); // ამატებს ერთ ელემენტს
+            //intList.AddRange(ar); // ამატებს რამდენიმე ელემენტს
+            //intList.Remove(10);
+            //intList.RemoveRange(0, 2); // წაშალე პირველი ორი ელემენტი ლისტში
+            //intList.Clear(); // ასუფთავებს ლისტს
+            //intList.Insert(0, 99); // ლისტის ნებისმიერ ინდექსზე ნებისმიერი მნიშვნელობის ჩამატება
+            //intList.InsertRange(0, [7, 7, 7]);
+            //bool contains = intList.Contains(7);
+            //int idx = intList.IndexOf(-10);
+            //intList.TrimExcess(); // გაამადგურებს ტყუილად გამოყოფილ ადგილებს
 
             //int listCount = intList.Count; // ზომა
             //int listCapacity = intList.Capacity; // მოცულობა
@@ -98,5 +108,7 @@
 
             #endregion
         }
+
+
     }
 }
