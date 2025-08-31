09991746f7f51189fe0527f3514e512a73269ad1
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2025-08-31 14:41:52 +0400

    Mapping

--- SE-4111-8/TinyBank.Service/Mapping/MapsterConfig.cs
+++ SE-4111-8/TinyBank.Service/Mapping/MapsterConfig.cs
@@ -1,5 +1,6 @@
 ﻿using Mapster;
 using TinyBank.Models;
+using TinyBank.Service.Dtos.Accounts;
 using TinyBank.Service.Dtos.Customer;
 
 namespace TinyBank.Service.Mapping
@@ -30,6 +31,21 @@ namespace TinyBank.Service.Mapping
                 .Map(dest => dest.PhoneNumber, src => src.PhoneNumber)
                 .Map(dest => dest.Email, src => src.Email)
                 .Map(dest => dest.CustomerType, src => src.CustomerType);
+
+            TypeAdapterConfig<Account, AccountForGettingDto>.NewConfig()
+                .Map(dest => dest.Id, src => src.Id)
+                .Map(dest => dest.Iban, src => src.Iban)
+                .Map(dest => dest.Currency, src => src.Currency)
+                .Map(dest => dest.Balance, src => src.Balance)
+                .Map(dest => dest.CustomerId, src => src.CustomerId)
+                .Map(dest => dest.Destination, src => src.Destination);
+
+            TypeAdapterConfig<AccountForCreatingDto, Account>.NewConfig()
+                .Map(dest => dest.Iban, src => src.Iban)
+                .Map(dest => dest.Currency, src => src.Currency)
+                .Map(dest => dest.Balance, src => src.Balance)
+                .Map(dest => dest.CustomerId, src => src.CustomerId)
+                .Map(dest => dest.Destination, src => src.Destination);
         }
     }
 }
