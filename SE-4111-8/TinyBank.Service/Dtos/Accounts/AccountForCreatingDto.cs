b121542f1ef0f085cca0b0c29b7f5e6ffbc0940a
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2025-08-31 14:40:55 +0400

    AccountForCreatingDto

---
+++ SE-4111-8/TinyBank.Service/Dtos/Accounts/AccountForCreatingDto.cs
@@ -0,0 +1,11 @@
+﻿namespace TinyBank.Service.Dtos.Accounts
+{
+    public record AccountForCreatingDto
+    {
+        public string Iban { get; set; }
+        public string Currency { get; set; }
+        public decimal Balance { get; set; }
+        public int CustomerId { get; set; }
+        public string Destination { get; set; }
+    }
+}
