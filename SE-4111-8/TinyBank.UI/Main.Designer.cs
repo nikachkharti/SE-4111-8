1c5bf25d7006d725cc5a1c08733a6c8e56a77227
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2025-08-31 11:43:09 +0400

    UI modifications

--- SE-4111-8/TinyBank.UI/Main.Designer.cs
+++ SE-4111-8/TinyBank.UI/Main.Designer.cs
@@ -28,20 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
             menuStrip1 = new MenuStrip();
+            homeToolStripMenuItem = new ToolStripMenuItem();
             customersToolStripMenuItem = new ToolStripMenuItem();
+            accountsToolStripMenuItem = new ToolStripMenuItem();
+            operationsToolStripMenuItem = new ToolStripMenuItem();
+            logoPictureBox = new PictureBox();
             menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)logoPictureBox).BeginInit();
             SuspendLayout();
             // 
             // menuStrip1
             // 
             menuStrip1.ImageScalingSize = new Size(24, 24);
-            menuStrip1.Items.AddRange(new ToolStripItem[] { customersToolStripMenuItem });
+            menuStrip1.Items.AddRange(new ToolStripItem[] { homeToolStripMenuItem, customersToolStripMenuItem, accountsToolStripMenuItem, operationsToolStripMenuItem });
             menuStrip1.Location = new Point(0, 0);
             menuStrip1.Name = "menuStrip1";
             menuStrip1.Size = new Size(1035, 33);
             menuStrip1.TabIndex = 0;
             menuStrip1.Text = "menuStrip1";
+            // 
+            // homeToolStripMenuItem
+            // 
+            homeToolStripMenuItem.Name = "homeToolStripMenuItem";
+            homeToolStripMenuItem.Size = new Size(77, 29);
+            homeToolStripMenuItem.Text = "Home";
+            homeToolStripMenuItem.Click += homeToolStripMenuItem_Click;
             // 
             // customersToolStripMenuItem
             // 
@@ -50,19 +63,44 @@
             customersToolStripMenuItem.Text = "Customers";
             customersToolStripMenuItem.Click += customersToolStripMenuItem_Click;
             // 
+            // accountsToolStripMenuItem
+            // 
+            accountsToolStripMenuItem.Name = "accountsToolStripMenuItem";
+            accountsToolStripMenuItem.Size = new Size(101, 29);
+            accountsToolStripMenuItem.Text = "Accounts";
+            // 
+            // operationsToolStripMenuItem
+            // 
+            operationsToolStripMenuItem.Name = "operationsToolStripMenuItem";
+            operationsToolStripMenuItem.Size = new Size(116, 29);
+            operationsToolStripMenuItem.Text = "Operations";
+            // 
+            // logoPictureBox
+            // 
+            logoPictureBox.Image = (Image)resources.GetObject("logoPictureBox.Image");
+            logoPictureBox.Location = new Point(240, 234);
+            logoPictureBox.Name = "logoPictureBox";
+            logoPictureBox.Size = new Size(535, 306);
+            logoPictureBox.SizeMode = PictureBoxSizeMode.CenterImage;
+            logoPictureBox.TabIndex = 1;
+            logoPictureBox.TabStop = false;
+            // 
             // Main
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = Color.White;
+            BackColor = Color.FromArgb(0, 43, 80);
             ClientSize = new Size(1035, 840);
+            Controls.Add(logoPictureBox);
             Controls.Add(menuStrip1);
+            FormBorderStyle = FormBorderStyle.FixedToolWindow;
             MainMenuStrip = menuStrip1;
             Name = "Main";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Main";
             menuStrip1.ResumeLayout(false);
             menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)logoPictureBox).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -71,5 +109,9 @@
 
         private MenuStrip menuStrip1;
         private ToolStripMenuItem customersToolStripMenuItem;
+        private ToolStripMenuItem accountsToolStripMenuItem;
+        private ToolStripMenuItem operationsToolStripMenuItem;
+        private ToolStripMenuItem homeToolStripMenuItem;
+        private PictureBox logoPictureBox;
     }
 }
--- SE-4111-8/TinyBank.UI/Main.cs
+++ SE-4111-8/TinyBank.UI/Main.cs
@@ -1,5 +1,4 @@
-﻿using TinyBank.Service.Implementations;
-using TinyBank.Service.Interfaces;
+﻿using TinyBank.Service.Interfaces;
 
 namespace TinyBank.UI
 {
@@ -15,9 +14,37 @@ namespace TinyBank.UI
 
         private void customersToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            var customersUserControl = new CustomersUC(_customerService);
-            customersUserControl.Dock = DockStyle.Fill;
-            this.Controls.Add(customersUserControl);
+            ShowUserControl(new CustomersUC(_customerService));
+            logoPictureBox.Visible = false;
         }
+
+
+        private void homeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ClearUserControls();
+            logoPictureBox.Visible = true;
+        }
+
+
+        private void ShowUserControl(UserControl control)
+        {
+            ClearUserControls();
+            control.Dock = DockStyle.Fill;
+            this.Controls.Add(control);
+            control.BringToFront();
+        }
+
+        private void ClearUserControls()
+        {
+            // Remove all user controls except the logoPictureBox and MenuStrip
+            for (int i = this.Controls.Count - 1; i >= 0; i--)
+            {
+                if (!(this.Controls[i] is PictureBox) && !(this.Controls[i] is MenuStrip))
+                {
+                    this.Controls.RemoveAt(i);
+                }
+            }
+        }
+
     }
 }
