c79560189efb38bfc54a64d54709d3021d99a775
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2025-08-31 10:55:48 +0400

    Customers User Control event bind

---
+++ SE-4111-8/TinyBank.UI/CustomersUC.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace TinyBank.UI
+{
+    partial class CustomersUC
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            customersList = new ListBox();
+            modifyGroupBox = new GroupBox();
+            clearFormButton = new Button();
+            deleteCustomerButton = new Button();
+            updateCustomerButton = new Button();
+            addCustomerButton = new Button();
+            customerTypeValue = new ComboBox();
+            customerTypeLabel = new Label();
+            emailLabel = new Label();
+            PhoneNumberLabel = new Label();
+            identityNumberLabel = new Label();
+            nameLabel = new Label();
+            emailValue = new TextBox();
+            phoneNumberValue = new TextBox();
+            identityNumberValue = new TextBox();
+            nameValue = new TextBox();
+            modifyGroupBox.SuspendLayout();
+            SuspendLayout();
+            // 
+            // customersList
+            // 
+            customersList.Dock = DockStyle.Top;
+            customersList.FormattingEnabled = true;
+            customersList.Location = new Point(0, 0);
+            customersList.Name = "customersList";
+            customersList.Size = new Size(1057, 279);
+            customersList.TabIndex = 0;
+            customersList.SelectedIndexChanged += customersList_SelectedIndexChanged;
+            // 
+            // modifyGroupBox
+            // 
+            modifyGroupBox.Controls.Add(clearFormButton);
+            modifyGroupBox.Controls.Add(deleteCustomerButton);
+            modifyGroupBox.Controls.Add(updateCustomerButton);
+            modifyGroupBox.Controls.Add(addCustomerButton);
+            modifyGroupBox.Controls.Add(customerTypeValue);
+            modifyGroupBox.Controls.Add(customerTypeLabel);
+            modifyGroupBox.Controls.Add(emailLabel);
+            modifyGroupBox.Controls.Add(PhoneNumberLabel);
+            modifyGroupBox.Controls.Add(identityNumberLabel);
+            modifyGroupBox.Controls.Add(nameLabel);
+            modifyGroupBox.Controls.Add(emailValue);
+            modifyGroupBox.Controls.Add(phoneNumberValue);
+            modifyGroupBox.Controls.Add(identityNumberValue);
+            modifyGroupBox.Controls.Add(nameValue);
+            modifyGroupBox.Location = new Point(10, 287);
+            modifyGroupBox.Name = "modifyGroupBox";
+            modifyGroupBox.Size = new Size(1039, 593);
+            modifyGroupBox.TabIndex = 1;
+            modifyGroupBox.TabStop = false;
+            modifyGroupBox.Text = "Modify";
+            // 
+            // clearFormButton
+            // 
+            clearFormButton.BackColor = Color.Gold;
+            clearFormButton.Cursor = Cursors.Hand;
+            clearFormButton.ForeColor = Color.Black;
+            clearFormButton.Location = new Point(519, 222);
+            clearFormButton.Name = "clearFormButton";
+            clearFormButton.Size = new Size(62, 34);
+            clearFormButton.TabIndex = 14;
+            clearFormButton.Text = "Clear";
+            clearFormButton.UseVisualStyleBackColor = false;
+            clearFormButton.Click += clearFormButton_Click;
+            // 
+            // deleteCustomerButton
+            // 
+            deleteCustomerButton.BackColor = Color.Crimson;
+            deleteCustomerButton.Cursor = Cursors.Hand;
+            deleteCustomerButton.ForeColor = Color.White;
+            deleteCustomerButton.Location = new Point(562, 317);
+            deleteCustomerButton.Name = "deleteCustomerButton";
+            deleteCustomerButton.Size = new Size(168, 34);
+            deleteCustomerButton.TabIndex = 15;
+            deleteCustomerButton.Text = "Delete Customer";
+            deleteCustomerButton.UseVisualStyleBackColor = false;
+            deleteCustomerButton.Click += deleteCustomerButton_Click;
+            // 
+            // updateCustomerButton
+            // 
+            updateCustomerButton.BackColor = Color.DeepSkyBlue;
+            updateCustomerButton.Cursor = Cursors.Hand;
+            updateCustomerButton.ForeColor = Color.Black;
+            updateCustomerButton.Location = new Point(388, 317);
+            updateCustomerButton.Name = "updateCustomerButton";
+            updateCustomerButton.Size = new Size(168, 34);
+            updateCustomerButton.TabIndex = 16;
+            updateCustomerButton.Text = "Update Customer";
+            updateCustomerButton.UseVisualStyleBackColor = false;
+            updateCustomerButton.Click += updateCustomerButton_Click;
+            // 
+            // addCustomerButton
+            // 
+            addCustomerButton.BackColor = Color.SeaGreen;
+            addCustomerButton.Cursor = Cursors.Hand;
+            addCustomerButton.ForeColor = Color.White;
+            addCustomerButton.Location = new Point(240, 317);
+            addCustomerButton.Name = "addCustomerButton";
+            addCustomerButton.Size = new Size(142, 34);
+            addCustomerButton.TabIndex = 17;
+            addCustomerButton.Text = "Add Customer";
+            addCustomerButton.UseVisualStyleBackColor = false;
+            addCustomerButton.Click += addCustomerButton_Click;
+            // 
+            // customerTypeValue
+            // 
+            customerTypeValue.Cursor = Cursors.Hand;
+            customerTypeValue.DropDownStyle = ComboBoxStyle.DropDownList;
+            customerTypeValue.FormattingEnabled = true;
+            customerTypeValue.Items.AddRange(new object[] { "Phyisical", "Legal" });
+            customerTypeValue.Location = new Point(519, 144);
+            customerTypeValue.Name = "customerTypeValue";
+            customerTypeValue.Size = new Size(258, 33);
+            customerTypeValue.TabIndex = 13;
+            // 
+            // customerTypeLabel
+            // 
+            customerTypeLabel.AutoSize = true;
+            customerTypeLabel.Location = new Point(519, 116);
+            customerTypeLabel.Name = "customerTypeLabel";
+            customerTypeLabel.Size = new Size(49, 25);
+            customerTypeLabel.TabIndex = 8;
+            customerTypeLabel.Text = "Type";
+            // 
+            // emailLabel
+            // 
+            emailLabel.AutoSize = true;
+            emailLabel.Location = new Point(255, 194);
+            emailLabel.Name = "emailLabel";
+            emailLabel.Size = new Size(54, 25);
+            emailLabel.TabIndex = 9;
+            emailLabel.Text = "Email";
+            // 
+            // PhoneNumberLabel
+            // 
+            PhoneNumberLabel.AutoSize = true;
+            PhoneNumberLabel.Location = new Point(255, 118);
+            PhoneNumberLabel.Name = "PhoneNumberLabel";
+            PhoneNumberLabel.Size = new Size(62, 25);
+            PhoneNumberLabel.TabIndex = 10;
+            PhoneNumberLabel.Text = "Phone";
+            // 
+            // identityNumberLabel
+            // 
+            identityNumberLabel.AutoSize = true;
+            identityNumberLabel.Location = new Point(519, 47);
+            identityNumberLabel.Name = "identityNumberLabel";
+            identityNumberLabel.Size = new Size(72, 25);
+            identityNumberLabel.TabIndex = 11;
+            identityNumberLabel.Text = "Identity";
+            // 
+            // nameLabel
+            // 
+            nameLabel.AutoSize = true;
+            nameLabel.Location = new Point(255, 47);
+            nameLabel.Name = "nameLabel";
+            nameLabel.Size = new Size(59, 25);
+            nameLabel.TabIndex = 12;
+            nameLabel.Text = "Name";
+            // 
+            // emailValue
+            // 
+            emailValue.Location = new Point(255, 222);
+            emailValue.Name = "emailValue";
+            emailValue.Size = new Size(258, 31);
+            emailValue.TabIndex = 4;
+            // 
+            // phoneNumberValue
+            // 
+            phoneNumberValue.Location = new Point(255, 146);
+            phoneNumberValue.Name = "phoneNumberValue";
+            phoneNumberValue.Size = new Size(258, 31);
+            phoneNumberValue.TabIndex = 5;
+            // 
+            // identityNumberValue
+            // 
+            identityNumberValue.Location = new Point(519, 75);
+            identityNumberValue.Name = "identityNumberValue";
+            identityNumberValue.Size = new Size(258, 31);
+            identityNumberValue.TabIndex = 6;
+            // 
+            // nameValue
+            // 
+            nameValue.Location = new Point(255, 75);
+            nameValue.Name = "nameValue";
+            nameValue.Size = new Size(258, 31);
+            nameValue.TabIndex = 7;
+            // 
+            // CustomersUC
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaption;
+            Controls.Add(modifyGroupBox);
+            Controls.Add(customersList);
+            Name = "CustomersUC";
+            Size = new Size(1057, 896);
+            Load += CustomersUC_Load;
+            modifyGroupBox.ResumeLayout(false);
+            modifyGroupBox.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ListBox customersList;
+        private GroupBox modifyGroupBox;
+        private Button clearFormButton;
+        private Button deleteCustomerButton;
+        private Button updateCustomerButton;
+        private Button addCustomerButton;
+        private ComboBox customerTypeValue;
+        private Label customerTypeLabel;
+        private Label emailLabel;
+        private Label PhoneNumberLabel;
+        private Label identityNumberLabel;
+        private Label nameLabel;
+        private TextBox emailValue;
+        private TextBox phoneNumberValue;
+        private TextBox identityNumberValue;
+        private TextBox nameValue;
+    }
+}
--- SE-4111-8/TinyBank.UI/Main.cs
+++ SE-4111-8/TinyBank.UI/Main.cs
@@ -1,146 +1,16 @@
-﻿using TinyBank.Models;
-using TinyBank.Service.Dtos;
-using TinyBank.Service.Interfaces;
+﻿using TinyBank.Service.Interfaces;
 
 namespace TinyBank.UI
 {
     public partial class Main : Form
     {
-        private readonly ICustomerService _customerService;
-
         public Main(ICustomerService customerService)
         {
             InitializeComponent();
-            _customerService = customerService;
+
+            var customersUserControl = new CustomersUC(customerService);
+            customersUserControl.Dock = DockStyle.Fill;
+            this.Controls.Add(customersUserControl);
         }
-
-        #region EVENTS
-        private void Main_Load(object sender, EventArgs e)
-        {
-            customersList.DataSource = GetCustomers();
-            ClearForm();
-        }
-        private void customersList_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            var selectedCustomer = customersList.SelectedItem as CustomerForGettingDto;
-            if (selectedCustomer != null)
-                FillForm(selectedCustomer);
-        }
-        private void addCustomerButton_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                var newCustomer = GenerateCreateForm();
-                _customerService.AddCustomer(newCustomer);
-                customersList.DataSource = GetCustomers();
-                ClearForm();
-
-                MessageBox.Show("Customer added successfully.");
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"Error adding customer: {ex.Message}");
-            }
-        }
-        private void clearFormButton_Click(object sender, EventArgs e) => ClearForm();
-        private void deleteCustomerButton_Click(object sender, EventArgs e)
-        {
-            var selectedCustomer = customersList.SelectedItem as CustomerForGettingDto;
-
-            if (selectedCustomer is null)
-            {
-                MessageBox.Show("Please select a customer to delete.");
-                return;
-            }
-
-            var confirmResult = MessageBox.Show(
-                $"Are you sure you want to delete '{selectedCustomer.Name}'?",
-                "Confirm Delete",
-                MessageBoxButtons.YesNo,
-                MessageBoxIcon.Warning);
-
-
-            if (confirmResult == DialogResult.Yes)
-            {
-                try
-                {
-                    _customerService.DeleteCustomer(selectedCustomer.Id);
-                    MessageBox.Show("Customer deleted successfully.");
-
-                    customersList.DataSource = GetCustomers();
-                    ClearForm();
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show($"Error deleting customer: {ex.Message}");
-                }
-            }
-
-        }
-
-        private void updateCustomerButton_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                var customerToUpdate = GenerateUpdateForm();
-                _customerService.UpdateCustomer(customerToUpdate);
-                customersList.DataSource = GetCustomers();
-                ClearForm();
-
-                MessageBox.Show("Customer updated successfully.");
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"Error updating customer: {ex.Message}");
-            }
-        }
-
-
-        #endregion
-
-
-        #region HELPERS
-        private List<CustomerForGettingDto> GetCustomers() => _customerService.GetAllCustomers();
-        private void FillForm(CustomerForGettingDto model)
-        {
-            nameValue.Text = model.Name;
-            identityNumberValue.Text = model.IdentityNumber;
-            phoneNumberValue.Text = model.PhoneNumber;
-            customerTypeValue.Text = model.CustomerType.ToString();
-            emailValue.Text = model.Email;
-        }
-        private void ClearForm()
-        {
-            nameValue.Text = string.Empty;
-            identityNumberValue.Text = string.Empty;
-            phoneNumberValue.Text = string.Empty;
-            customerTypeValue.Text = string.Empty;
-            emailValue.Text = string.Empty;
-        }
-        private CustomerForCreatingDto GenerateCreateForm() => new CustomerForCreatingDto()
-        {
-            Name = nameValue.Text,
-            IdentityNumber = identityNumberValue.Text,
-            PhoneNumber = phoneNumberValue.Text,
-            CustomerType = Enum.Parse<CustomerType>(customerTypeValue.Text),
-            Email = emailValue.Text
-        };
-        private CustomerForUpdatingDto GenerateUpdateForm()
-        {
-            var selectedCustomer = customersList.SelectedItem as CustomerForGettingDto;
-
-            return new CustomerForUpdatingDto()
-            {
-                Id = selectedCustomer.Id,
-                Name = nameValue.Text,
-                IdentityNumber = identityNumberValue.Text,
-                PhoneNumber = phoneNumberValue.Text,
-                CustomerType = Enum.Parse<CustomerType>(customerTypeValue.Text),
-                Email = emailValue.Text
-            };
-        }
-        #endregion
-
-
     }
 }
